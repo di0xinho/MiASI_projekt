26100513cc8e032c1ace6a1ece25a5d5d1df18b8
WojciechKuz <104651306+WojciechKuz@users.noreply.github.com>
2024-07-10 18:11:38 +0200

    listelement todo

---
+++ listelement.py
@@ -0,0 +1,24 @@
+
+
+
+# to będzie klasa, która umożliwi wyświetlenie jakiejś formuły mat (wyniki z historii obliczeń / funkcje)
+# jako element listy.
+
+# ogólne opcje:
+# - wyświetlanie formuły, 1 lub 2
+# - stały tekst na początku, jak np. f(x)=
+# - możliwość dołożenia przycisków / dropdown
+# - opcja usuń
+
+# Usuń, możliwe rozwiązania: Albo prawym i wybrać usuń, albo przycisk z symbolem kosza albo X.
+# albo dropdown menu pod prawym przyciskiem, albo przyciski z ikonkami
+
+# te mogą być w oddzielnych dziedziczących z tego klasach:
+
+# opcje historii:
+# - kliknij, aby skopiować wynik
+
+# opcje listy wykresów:
+# - checkbox - kontroluje widoczność na wykresie. Może zamiast tego wyszarzanie pod lewym przyciskiem myszy?
+# - edytuj (button lub w dropdownie)
+# - może pole z kolorem?
